604a7bf51766a0dc3b6186f6587f735a0a5710e4
hmanjun <harshith.hm03@gmail.com>
2023-02-08 11:27:37 -0800

    Add base server.js file

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const https = require("https")
+const cors = require('cors')
+
+//const db = require("./config/connection")
+const PORT = 8080
+
+const app = express()
+
+//Middleware to parse incoming data format
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+//Allow cross-origin access
+app.use(
+    cors({
+        origin: "*" //Update to match the domain you will make requests from
+    })
+)
+
+const server = new https.createServer(app)
+
+db.once('open', () => {
+    server.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}`)
+    })
+})
